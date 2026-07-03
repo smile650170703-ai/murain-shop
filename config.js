5e90189b4c6dd72800f35ae6f62f2120a14a8629
smile650170703-ai <smile650170703@gmail.com>
2026-07-03 22:52:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@
     : '2008430261-kOeNLR9x';
 window.OFFICIAL_LINE_URL = isStaging
   ? 'https://line.me/R/ti/p/@523fqjkm'
-  : 'https://line.me/R/ti/p/@cc0857';
+  : 'https://line.me/R/ti/p/@mu88888';
 
 // ✅ 寄賣契約專用 LINE：只給 consign.html 的契約按鈕使用
 window.CONSIGN_CONTRACT_LINE_URL = isStaging
